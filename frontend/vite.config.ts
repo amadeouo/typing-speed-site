41b36be9eeaed944130ddad9d01fc14c74b174af
Mikhail Volodko <clavor.32@gmail.com>
2026-02-18 21:18:22 +0300

    init: init proj

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,19 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+
+  return {
+    // server: {
+    //   host: true,
+    // },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    base: isProd ? '/WeatherApp/' : '/',
+  }
 })
